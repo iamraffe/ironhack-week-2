fea59eabd53962fbb84bd1313c6efcae34ece654
Rafa Ramírez <raffe90@gmail.com>
2015-10-17 10:19:30 +0200

    New version of car.rb old version renamed to friday.car.rb

--- car.rb
+++ car.rb
@@ -1,56 +1,30 @@
 class Car
-	def initialize(noise, storage = "default.txt")
+	def initialize(noise, engine)
 		@noise = noise
-		@storage = storage
-		# @cities_visited = []
+		@engine = engine
 	end
 
 	def make_noise
-		puts @noise
-	end
-
-	def make_noise_twice
-		2.times {make_noise}
-	end
-
-	def self.make_noise_twice(car)
-		2.times {car.make_noise}
-	end
-
-	def visit(city)
-		# @cities_visited.push(city)
-		File.open(@storage, 'a') { |f| f.write("#{city}\n") }
-	end
-
-	def view_visited_cities
-		#puts @cities_visited
-		puts IO.read(@storage);
+		puts "#{@noise} #{@engine.make_noise}"
 	end
 end
 
-# class City
-# 	def initialize(name)
-# 		@name = name
-# 	end
-# end
-
-class RacingCar < Car
+class Engine
 	def make_noise
-		puts @noise.upcase
+		"fishum fishum"
 	end
 end
 
-noisy_car = Car.new("BROOOOOOM!!!!!!PiuPiuPiu!!!!!BROOOOOOM", "noisy.txt")
-sleepy_car = RacingCar.new("ZzZzZzZ", "sleepy.txt")
+class SadEngine
+	def make_noise
+		"*cryes*"
+	end
+end
 
-# noisy_car.make_noise
+
+car = Car.new("Broom", Engine.new)
+sad_car = Car.new("mimimimimi", SadEngine.new)
+
 # car.make_noise
-# Car.make_noise_twice(car)
-# sleepy_car.make_noise_twice
 
-# sleepy_car.make_noise
-
-sleepy_car.visit("Seattle")
-sleepy_car.visit("New York")
-
-sleepy_car.view_visited_cities
+sad_car.make_noise
